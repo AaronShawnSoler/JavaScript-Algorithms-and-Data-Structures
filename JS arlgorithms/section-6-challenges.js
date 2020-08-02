0d8c1a117df5eaa6c3f21f1454e0fc0978e95847
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-08-02 19:52:15 -0400

    get args from parameters

---
+++ JS arlgorithms/section-6-challenges.js
@@ -0,0 +1,11 @@
+//==========================================
+// Frequency Counter / Multiple Pointers - areThereDuplicates
+//==========================================
+
+function areThereDuplicates() {
+
+    const args = Array.from(arguments)
+    args.forEach(arg => console.log(arg))
+}
+
+areThereDuplicates(1,2,3)
