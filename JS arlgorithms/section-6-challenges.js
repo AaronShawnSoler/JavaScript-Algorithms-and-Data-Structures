ea2ec5514d63a56e8ba1ecb6052f07b58944bd01
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-08-07 00:47:39 -0400

    min sub array len

--- JS arlgorithms/section-6-challenges.js
+++ JS arlgorithms/section-6-challenges.js
@@ -148,3 +148,20 @@ maxSubarraySum([1,4,2,10,23,3,1,0,20], 4) // 39
 maxSubarraySum([-3,4,0,-2,6,-1], 2) // 5
 maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2) // 5
 maxSubarraySum([2,3], 3) // null
+
+
+//==========================================
+// Sliding Window - minSubArrayLen
+//==========================================
+
+function minSubArrayLen(arr, target) {
+
+}
+
+minSubArrayLen([2,3,1,2,4,3], 7) // 2
+minSubArrayLen([2,1,6,5,4], 9) // 2
+minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) // 1
+minSubArrayLen([1,4,16,22,5,7,8,9,10], 39) // 3
+minSubArrayLen([1,4,16,22,5,7,8,9,10], 55) // 5
+minSubArrayLen([4,3,3,8,1,2,3], 11) // 2
+minSubArrayLen([,1,4,16,22,5,7,8,9,10], 95) // 0
