f59b5d740c698fd14a2e669bb80dcb5099ba2d12
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-08-10 17:35:06 -0400

    section 8

--- JS arlgorithms/section-6-challenges.js
+++ JS arlgorithms/section-6-challenges.js
@@ -155,7 +155,30 @@ maxSubarraySum([2,3], 3) // null
 //==========================================
 
 function minSubArrayLen(arr, target) {
+    
+    let total = 0
+    let start = 0
+    let end = 0
+    let minLen = Infinity
 
+    while(start < arr.length) {
+
+        // console.log(`Total: ${total}`, `Start: ${start}`, `End: ${end}`, `MinLen: ${minLen}`)
+
+        if(total < target && end < arr.length) {
+            total += arr[end]
+            end++
+        } else if(total >= target) {
+            minLen = Math.min(minLen, end-start)
+            total -= arr[start]
+            start++
+        } else {
+            break;
+        }
+    }
+
+    // console.log('====================================================', target, minLen === Infinity ? 0 : minLen)
+    return minLen === Infinity ? 0 : minLen
 }
 
 minSubArrayLen([2,3,1,2,4,3], 7) // 2
