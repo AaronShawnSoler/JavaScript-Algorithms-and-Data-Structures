cd780060bc53757a0c46823d05565eea6b320820
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-08-16 05:37:21 -0400

    selection sort

--- JS arlgorithms/sorting.js
+++ JS arlgorithms/sorting.js
@@ -12,6 +12,7 @@ function bubbleSort(arr) {
         // scan array and see if previous element is greater than next element
         // if so swap
         for(let index = 0; index < arr.length - 1; index ++) {
+            // (Flip sign to sort descending)
             if(arr[index] > arr[index + 1]) {
                 [arr[index], arr[index + 1]] = [arr[index + 1], arr[index]]
                 swapped = true
@@ -25,8 +26,37 @@ function bubbleSort(arr) {
         swap()
     } while(swapped == true) swap()
     
+    return arr
+}
+
+// console.log(bubbleSort([2,1,5,3,4,0,-5,-1,-2,-4,-3])) // [1,2,3,4,5]
+
+// =====================================================
+// Selection Sort
+// =====================================================
+
+function selectionSort(arr) {
+    
+    // sort each item
+    for(let sort = 0; sort < arr.length; sort++) {
+        let smallest = sort
+
+        // find smallest item after curr sorted item
+        for(let index = sort + 1; index < arr.length; index++) {
+            if(arr[index] < arr[smallest]) {
+                smallest = index
+            }
+        }
+        
+        // if curr sorted item isnt the smallest swap
+        if(sort != smallest) {
+            let temp = arr[sort]
+            arr[sort] = arr[smallest]
+            arr[smallest] = temp
+        }
+    }
 
     return arr
 }
 
-console.log(bubbleSort([2,1,5,3,4])) // [1,2,3,4,5]
+// console.log(selectionSort([2,1,5,3,4,0,-5,-1,-2,-4,-3])) // [1,2,3,4,5]
