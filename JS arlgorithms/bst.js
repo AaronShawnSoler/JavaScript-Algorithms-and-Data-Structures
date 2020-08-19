8a452bf692c1610375a57d6b882865417c44816e
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-08-19 17:47:09 -0400

    bst setup

---
+++ JS arlgorithms/bst.js
@@ -0,0 +1,14 @@
+class BTNode {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+}
+
