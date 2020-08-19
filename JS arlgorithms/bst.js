0c9999266b530646f13f5e6e5f59c0f108788e36
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-08-19 18:33:57 -0400

    insert

--- JS arlgorithms/bst.js
+++ JS arlgorithms/bst.js
@@ -1,6 +1,7 @@
 class BTNode {
     constructor(val) {
         this.val = val
+        this.count = 1
         this.left = null
         this.right = null
     }
@@ -10,5 +11,37 @@ class BinarySearchTree {
     constructor() {
         this.root = null
     }
+
+    insert(val) {
+        const newNode = new BTNode(val)
+
+        if(!this.root) {
+            this.root = newNode
+        } else {
+            let curr = this.root
+
+            while(true) {
+                if(val > curr.val) {
+                    if(curr.right) {
+                        curr = curr.right
+                    } else {
+                        curr.right = newNode
+                        return newNodea
+                    }
+                } else if(val < curr.val) {
+                    if(curr.left) {
+                        curr = curr.left
+                    } else {
+                        curr.left = newNode
+                        return newNode
+                    }
+                } else {
+                    curr.count++
+                    return newNode
+                }
+            }
+        }
+    }
 }
 
+const btree = new BinarySearchTree()
