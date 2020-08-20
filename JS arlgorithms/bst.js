2eea544a268bc73dc886ebd1f64cfb9311b726d2
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-08-19 21:05:33 -0400

    bfs

--- JS arlgorithms/bst.js
+++ JS arlgorithms/bst.js
@@ -26,7 +26,7 @@ class BinarySearchTree {
                         curr = curr.right
                     } else {
                         curr.right = newNode
-                        return newNodea
+                        return newNode
                     }
                 } else if(val < curr.val) {
                     if(curr.left) {
@@ -42,6 +42,60 @@ class BinarySearchTree {
             }
         }
     }
+
+    find(val) {
+        if(!this.root) {
+            return undefined
+        } else {
+            let curr = this.root
+
+            while(true) {
+                if(val == curr.val) return true
+
+                if(val > curr.val) {
+                    if(curr.right) {
+                        curr = curr.right
+                    } else {
+                        return false
+                    }
+                } else if(val < curr.val) {
+                    if(curr.left) {
+                        curr = curr.left
+                    } else {
+                        return false
+                    }
+                }
+            }
+        }
+    }
+
+    bfs() {
+        if(!this.root) return undefined
+
+        const q = new Queue()
+        q.enqueue(this.root)
+
+        let visited = []
+
+        while(q.size > 0) {
+            let node = q.dequeue()
+            if(node.left) q.enqueue(node.left)
+            if(node.right) q.enqueue(node.right)
+            visited.push(node.val)
+        }
+
+        return visited
+    }
 }
 
 const btree = new BinarySearchTree()
+
+btree.insert(10)
+
+btree.insert(5)
+btree.insert(20)
+
+btree.insert(1)
+btree.insert(6)
+btree.insert(15)
+btree.insert(21)
