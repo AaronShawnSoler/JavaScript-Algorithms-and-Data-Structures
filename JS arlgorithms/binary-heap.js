d25ea497bfc357a942d5b43abf66ce50b5522bc0
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-08-23 01:44:28 -0400

    comments

--- JS arlgorithms/binary-heap.js
+++ JS arlgorithms/binary-heap.js
@@ -19,7 +19,9 @@ class BinaryHeap {
     }
 
     extractMax() {
+        // swap root with last element
         [this.heap[0], this.heap[this.heap.length - 1]] = [this.heap[this.heap.length - 1], this.heap[0]]
+        // save last element (root) for later
         let retVal = this.heap.pop()
 
         let parent = 0
@@ -28,13 +30,16 @@ class BinaryHeap {
         let left = this.heap[leftIdx]
         let right = this.heap[rightIdx]
 
+        //while left or right child is greater than parent
         while(left > this.heap[parent] || right > this.heap[parent]) {
             leftIdx = 2 * parent + 1
             rightIdx = 2 * parent + 2
             left = this.heap[leftIdx]
             right = this.heap[rightIdx]
 
+            // if left and right child are greater
             if(left > this.heap[parent] && right > this.heap[parent]) {
+                // swap child that is greater than that other
                 if(left > right) {
                     [this.heap[parent], this.heap[leftIdx]] = [this.heap[leftIdx], this.heap[parent]]
                     parent = 2 * parent + 1
@@ -42,9 +47,11 @@ class BinaryHeap {
                     [this.heap[parent], this.heap[rightIdx]] = [this.heap[rightIdx], this.heap[parent]]
                     parent = 2 * parent + 2
                 }
+            // swap left child if its greater
             } else if(left > this.heap[parent]) {
                 [this.heap[parent], this.heap[leftIdx]] = [this.heap[leftIdx], this.heap[parent]]
                 parent = 2 * parent + 1
+            // swap right child if its greater
             } else if(right > this.heap[parent]) {
                 [this.heap[parent], this.heap[rightIdx]] = [this.heap[rightIdx], this.heap[parent]]
                 parent = 2 * parent + 2
